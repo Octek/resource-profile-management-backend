1bcd449e2a51ff9a8ae0dcabdc0e8850de5bc451
Abdullah-khalid <abdullah.khalid206@gmail.com>
2024-12-03 18:01:18 +0500

    changed the name of cc toggle

--- utils/env_variable_function.go
+++ utils/env_variable_function.go
@@ -16,7 +16,7 @@ const (
 	SENDER_PASSWORD              = "SENDER_PASSWORD"
 	CC_EMAILS                    = "CC_EMAILS"
 	TO_EMAIL                     = "TO_EMAIL"
-	CC_TOGGLE                    = "CC_TOGGLE"
+	SEND_CC_EMAIL                = "SEND_CC_EMAIL"
 )
 
 func GetConnectionString() string {
@@ -96,9 +96,9 @@ func GetToEmail() string {
 }
 
 func GetCCToggle() string {
-	ccToggle, ok := os.LookupEnv(CC_TOGGLE)
+	ccToggle, ok := os.LookupEnv(SEND_CC_EMAIL)
 	if !ok {
-		panic(CC_TOGGLE + EnvironmentVariableNotSet)
+		panic(SEND_CC_EMAIL + EnvironmentVariableNotSet)
 	}
 	return ccToggle
 }
